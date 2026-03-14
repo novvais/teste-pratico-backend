c3a862f8bcfc2e72807fe6cf9718c4f27ac17d6c
novvais <eduardodepaulanovais10@gmail.com>
2026-03-13 22:37:42 -0300

    FT: CRUD completo

---
+++ multi-gateways/app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'sometimes',
+                'required',
+                'email:rfc,dns',
+                Rule::unique('users')->ignore($this->route('id'))
+            ],
+            'password' => 'sometimes|required|string|min:8'
+        ];
+    }
+}
